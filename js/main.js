e47725fa299964082844a0b80067db00c782fccc
Emmanuel Lemal <manuman73@gmail.com>
2022-11-29 12:21:04 +0100

    tuto jusque toggleClass

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
-function init(){
+// function init(){
     
-}
+// }
 
-window.addEventListener('load', function(){
-    init();
-});
+// window.addEventListener('load', function(){
+//     init();
+// });
+
+
+// A $( document ).ready() block.
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+// gsap.to("#intro img", {
+    
+//     opacity:0,
+//     scrollTrigger: {
+//         trigger: '#intro h1',
+//         start:'center 300px',
+//         end:'bottom 200px',
+//         scrub: true,
+//         markers:true
+//     }
+//     })
+
+
+gsap.set("#project02", {
+    scrollTrigger: {
+
+        trigger:'#project02 img',
+        start:'0% 80%',
+        end: '100% 50%',
+        markers: true,
+        toggleClass: {
+            targets: "#project02",
+            className: "active" }
+
+    }})
+
+
+
+
+
+}); //ready
